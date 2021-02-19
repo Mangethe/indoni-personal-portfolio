ff3f6fab607cff421a2c9a463e593761508125e4
Nondumiso Ngcobo <ngcobonondumiso27@gmail.com>
2021-02-20 00:17:32 +0200

    Styling in progress

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/services">Services</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/projects">Projects</router-link> |
       <router-link to="/contact">Contact</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
