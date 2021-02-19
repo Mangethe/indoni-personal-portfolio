68ad810d5141ef01e297f083d6a50df442fa9046
CarlaLlama <carla@reflectivelearning.co.za>
2021-02-20 00:11:24 +0200

    Env var changes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="home">
     <h1>Contact Me</h1>
-    <form action="">
-      <input type="text" placeholder="Enter your name">
-      <input type="text" placeholder="Your email address">
+    <form action="handleSubmit">
+      <input type="text" placeholder="Enter your name" />
+      <input type="text" placeholder="Your email address" />
+      <input type="text" placeholder="Your message" />
     </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Axios from "axios";
 
 export default {
-  name: "ContactView"
+  name: "ContactView",
+  data() {
+    return {
+      name: null,
+      email: null,
+      message: null
+    };
+  },
+  methods: {
+    handleSubmit() {
+      event.preventDefault();
+
+      const data = {
+        email: this.email,
+        message: this.message,
+        name: this.name
+      };
+
+      Axios.post(
+        `https://us-central1-${process.env.VUE_APP_FIREBASE_PROJECT_ID}.cloudfunctions.net/submit`,
+        data
+      )
+        .then(res => {
+          console.log("res", res);
+          // here will be code
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
