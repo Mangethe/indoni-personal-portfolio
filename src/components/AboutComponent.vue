5f52e6001b7367de3bdc5159ddf630ebff1c44d6
CarlaLlama <carla@reflectivelearning.co.za>
2021-02-20 06:48:43 +0200

    Merge conflict

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,14 +1,19 @@
 <template>
   <!-- about -->
-<section id="About" class="section">
-  <div class="container">
-    <div class="columns is-centered">
-      <div class="column is-four-fifths-desktop has-text-centered">
-        <p class="font-secondary paragraph-lg has-text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        <a href="about.html" class="button is-link">know more</a>
+  <section id="About" class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-four-fifths-desktop has-text-centered">
+          <p class="font-secondary paragraph-lg has-text-dark">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+          <a href="about.html" class="button is-link">know more</a>
+        </div>
       </div>
     </div>
   </section>
